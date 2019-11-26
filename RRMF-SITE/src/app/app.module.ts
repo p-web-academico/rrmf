f451d18e2767514221a969fe4af1cdc77622c31f
p-web-academico <p.web.academico@gmail.com>
2019-11-26 19:59:42 -0300

    Install Firebase and Conection

--- RRMF-SITE/src/app/app.module.ts
+++ RRMF-SITE/src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +15,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth feature
   ],
   providers: [],
   bootstrap: [AppComponent]
--- RRMF-SITE/src/environments/environment.ts
+++ RRMF-SITE/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBS7CYwkt_L8sb8k-DyN2lu_BO5cpxcE1M",
+    authDomain: "rrmf-login.firebaseapp.com",
+    databaseURL: "https://rrmf-login.firebaseio.com",
+    projectId: "rrmf-login",
+    storageBucket: "rrmf-login.appspot.com",
+    messagingSenderId: "915859804660",
+    appId: "1:915859804660:web:7dc04d845e4c6df6fde9a1"
+  }
 };
 
 /*
